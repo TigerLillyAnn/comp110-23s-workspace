36c81321097872010f5fa55e9469c9c8754eb11a
LillyAnn Nekervis <122558939+TigerLillyAnn@users.noreply.github.com>
2023-01-24 22:45:51 -0500

    Add files via upload

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,48 @@
+"""EX01 - Chardle - A cute step towards Wordle!"""
+__author__ = 730477496
+
+Word: str = input("Enter a 5-character word:")
+Letter: str = input("Enter a single character:")
+
+if 5 < len(Word) > 5 : 
+    print("Error: Word must contain 5 letters")
+    exit()
+if 2 < len(Letter) > 2 : 
+    print("Error: Character must be a single character.")
+    exit()
+
+print("Searching for " + Letter + " in " + Word)
+
+x: int = 0
+
+if 5 < len(Word) > 5: 
+    print("Error: Word must contain 5 letters")
+
+if (Word[0] == Letter): 
+    print(Letter + " found at index 0")
+    x = x + 1
+
+if (Word[1] == Letter):
+    print(Letter + " found at index 1")
+    x = x + 1
+
+if (Word[2] == Letter):
+    print(Letter + " found at index 2")
+    x = x + 1
+
+if (Word[3] == Letter):
+    print(Letter + " found at index 3")
+    x = x + 1
+
+if (Word[4] == Letter):
+    print(Letter + " found at index 4") 
+    x = x + 1 
+
+if (x == 1):
+    print(x, "instance of " + Letter + " found in " + Word)
+
+if (x == 0):
+    print("No instances of " + Letter + " found in " + Word)
+
+if (x >= 2):
+        print(x, "instances of " + Letter + " found in " + Word)
