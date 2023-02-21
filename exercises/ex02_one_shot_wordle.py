17d8a9da0694ac8b634190993075486a7a8cfce5
LillyAnn <lnekervis@unc.edu>
2023-02-21 18:22:53 -0500

    update

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,69 +1,24 @@
 __author__ = "730477496"
 
-secretword = "python"
+word: str = input("What is your 6-letter guess? ")
+while len(word) != len("python"):
+    word = input("That was not 6 letters! Try again: ")
 
-Word: str = input("What is your 6-letter guess? ")
-correct = "\U0001F7E9"
-correctletter = "\U0001F7E8"
-incorrect = "\U00002B1C"
-
-if Word == secretword:
+if word == "python":
     print("Woo! You got it!")
     print("\U0001F7E9" + "\U0001F7E9" + "\U0001F7E9" + "\U0001F7E9" + "\U0001F7E9" + "\U0001F7E9")
+    exit
 
-emojis = ""
+i = int = 0
 
-while Word != secretword:
-    if len(Word) != 6: 
-        Word: str = input("That was not 6 letters! Try again: ")
-    if Word == secretword:
-        print("Woo! You got it!")
-        print("\U0001F7E9" + "\U0001F7E9" + "\U0001F7E9" + "\U0001F7E9" + "\U0001F7E9" + "\U0001F7E9")
-        exit
-    else: 
-        print("Not quite. Play again soon!") 
-        'a' 
-        if Word[0] == secretword : 
-            emojis += ("\U0001F7E9")
-        elif Word[0] in secretword : 
-            emojis += ("\U0001F7E8")
-        else : 
-            emojis += ("\U00002B1C") 
-        
-        if Word[1] == secretword : 
-            emojis += ("\U0001F7E9")
-        elif Word[1] in secretword : 
-            emojis += ("\U0001F7E8")
-        else : 
-            emojis += ("\U00002B1C") 
-
-        if Word[2] == secretword : 
-            emojis += ("\U0001F7E9")
-        elif Word[2] in secretword : 
-            emojis += ("\U0001F7E8")
-        else : 
-            emojis += ("\U00002B1C") 
-
-        if Word[3] == secretword : 
-            emojis += ("\U0001F7E9")
-        elif Word[3] in secretword : 
-            emojis += ("\U0001F7E8")
-        else : 
-            emojis += ("\U00002B1C") 
-
-        if Word[4] == secretword : 
-            emojis += ("\U0001F7E9")
-        elif Word[4] in secretword : 
-            emojis += ("\U0001F7E8")
-        else : 
-            emojis += ("\U00002B1C") 
-
-        if Word[5] == secretword : 
-            emojis += ("\U0001F7E9") 
-        elif Word[5] in secretword : 
-            emojis += ("\U0001F7E8")
-        else : 
-            emojis += ("\U00002B1C") 
-        print(emojis)
-        break 
-    
+while i < len("python"):
+    if i == "python" :
+        print("\U0001F7E9")
+        i = i + 1 
+    else:
+        if word[i] in "python":
+            print("\U0001F7E8")
+            i = i + 1
+        else:
+            print("\U00002B1C")
+            i = i + 1
