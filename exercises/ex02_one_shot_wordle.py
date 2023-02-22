3f7740cd6be41066db1bb152a8662947a27e28e9
LillyAnn <lnekervis@unc.edu>
2023-02-21 22:38:58 -0500

    UPDATE

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -3,9 +3,11 @@ __author__ = "730477496"
 GREEN_BOX: str = "\U0001F7E9"
 GRAY_BOX: str = "\U00002B1C"
 YELLOW_BOX: str = "\U0001F7E8"
+BOXES = ""
 
 secret: str = "python"
 word: str = input("What is your 6-letter guess? ")
+
 while len(word) != len(secret):
     word = input("That was not 6 letters! Try again: ")
 
@@ -13,18 +15,17 @@ i: int = 0
 
 while i < len(secret):
     if word[i] == secret[i]:
-        print(GREEN_BOX)
+        BOXES += GREEN_BOX
     else:
         j: int = 0
         in_secret: bool = False
         while j < len(secret):
             if secret[j] == word[i]:
                 in_secret = True
-            
             j += 1
         if in_secret:
-            print(YELLOW_BOX)
+            BOXES += YELLOW_BOX
         else:
-            print(GRAY_BOX)
+            BOXES += GRAY_BOX
     i = i + 1 
-
+print(BOXES)
