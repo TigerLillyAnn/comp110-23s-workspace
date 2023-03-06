3c0a1818dccec02b1da8459a7e9e7d99ed64a4b9
LillyAnn <lnekervis@unc.edu>
2023-03-06 11:46:29 -0500

    CQ for Sum, ex04 modification

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -16,27 +16,15 @@ def all(ints, insert: int) -> bool:
 print(all([1, 2, 3], 2)) #prints False
 print(all([2, 2, 2], 2)) #prints True
 
-def max(ints, insert: int) -> bool:
-    n: int = len(ints)
-    j: int = 0
-    while j < n:
-        if ints[j] == insert:
-            j = j + 1
-        else:
-            j = j + 1
+def max(numbers):
+    if len(numbers) == 0:
+        raise ValueError("List is empty.")
+    return max(numbers)
+
+def is_equal(list1, list2):
+    if len(list1) != len(list2):
+        return False
+    for i in range(len(list1)):
+        if list1[i] != list2[i]:
             return False
     return True
-
-def is_equal(ints, insert:int) -> bool:
-    count: int = len(ints)
-    count2 = 0
-    output: bool = False
-    if len(input) == 0:
-        return False
-    while count < len(input):
-        if input(count) == insert(count2):
-            output = True
-        else: return False 
-        count += 1
-        count2 += 1
-    return(output)
