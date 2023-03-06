1e795fa89d9a05557c4a04e577ac6ed3269f8d05
LillyAnn <lnekervis@unc.edu>
2023-03-06 11:53:41 -0500

    changed for/in for while

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -2,19 +2,13 @@
 ___author___ = "730477496"
 
 
-def all(ints, insert: int) -> bool:
-    n: int = len(ints)
-    j: int = 0
-    while j < n:
-        if ints[j] == insert:
-            j = j + 1
-        else:
-            j = j + 1
-            return False
-    return True
-
-print(all([1, 2, 3], 2)) #prints False
-print(all([2, 2, 2], 2)) #prints True
+def all(numbers, target):
+    i: int = 0
+    numbers: list[int] = [1, 2, 3]
+    if not numbers:
+        return False
+    else:
+        return True
 
 def max(numbers):
     if len(numbers) == 0:
@@ -24,7 +18,5 @@ def max(numbers):
 def is_equal(list1, list2):
     if len(list1) != len(list2):
         return False
-    for i in range(len(list1)):
-        if list1[i] != list2[i]:
-            return False
-    return True
+    else:
+        return True
