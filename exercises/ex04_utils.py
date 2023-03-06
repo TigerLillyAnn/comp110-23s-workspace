6232014d2f0532c4911b68671dec1fb8af0fd95f
LillyAnn <lnekervis@unc.edu>
2023-03-06 13:46:25 -0500

    Updates to def in ex04

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,20 +1,31 @@
 """Lists exercise!"""
 ___author___ = "730477496"
 
-
-def all(numbers, target):
-    if not numbers:
-        return False
-    return numbers.count(target) == len(numbers)
-#using count for smaller list recommended since it goes through the sequence twice.
+def all(ints, insert: int) -> bool:
+    n: int = len(ints)
+    j: int = 0
+    while j < n:
+        if ints[j] == insert:
+            return True
+        j = j + 1
+    return False
 
 def max(numbers):
     if len(numbers) == 0:
         raise ValueError("List is empty.")
     return max(numbers)
 
-def is_equal(list1, list2):
-    if len(list1) != len(list2):
+def is_equal(ints, insert:int) -> bool:
+    count: int = 0
+    count2: int = 0
+    output: bool = False
+    if len(ints) != len(insert):
         return False
-    else:
-        return True
+    while count2 < len(insert):
+        if ints[count] == insert[count2]:
+            output = True
+        else:
+            return False 
+        count += 1
+        count2 += 1
+    return output
