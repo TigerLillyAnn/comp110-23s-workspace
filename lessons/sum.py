384d6707509795984382e77fdd3e4aa829842ae9
LillyAnn <lnekervis@unc.edu>
2023-03-06 11:46:29 -0500

    CQ for Sum, ex04 modification

--- lessons/sum.py
+++ lessons/sum.py
@@ -1,11 +1,12 @@
 """Example function for unit tests"""
 
 def sum(xs: list[float]) -> float:
-    """return sum off all elements in xs"""
+    """return sum of all elements in xs"""
+    if len(xs) == 1:
+        return xs[0]
+    
     sum_total: float = 0.0
-    idx: int = 0
-    for idx in sum_total:
-        while idx < len(xs):
-            sum_total += xs[idx]
-            idx += 1
-        return 0.0
+    for num in range(0,len(xs)):
+        sum_total += xs[num]
+
+    return sum_total
