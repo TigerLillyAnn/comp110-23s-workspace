ac5211cccadd92c88c82efc1a74e0c40d5d5bf65
LillyAnn <lnekervis@unc.edu>
2023-03-08 01:16:57 -0500

    condense utils_test

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,7 @@
+"""Exercise 5: Utility functions list"""
 __author__ = "730477496"
 
+"""Checks list for even numbers, output is only even numbers"""
 def only_evens(i: list[int]):
     even = []
     for num in i:
@@ -7,8 +9,10 @@ def only_evens(i: list[int]):
             even.append(num)
     return even
 
+"""prints a combination of first list + second list"""
 def concat(first: list[int], second: list[int]):
     return first + second 
 
+"""takes the start, end index of the list"""
 def sub(first: list[int], start_index: list[int], end_index: list[int]):
     return first[start_index: end_index]
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,48 +1,39 @@
+"""Asserting list and utility functions"""
 ___author___ = "730477496"
 
 from exercises.ex05.utils import only_evens, concat, sub
 
 def test_only_evens():
-    test_list: list[int] = [2, 3, 4, 5]
-    assert only_evens(test_list) == [2, 4]
+    test_list_1: list[int] = [2, 3, 4, 5]
+    assert only_evens(test_list_1) == [2, 4]
+    test_list_2: list[int] = [50, 60, 70, 80, 90, 100]
+    assert only_evens(test_list_2) == [60, 80, 100]
+    test_list_3: list[int] = [5, 7, 9]
+    assert only_evens(test_list_3) == []
 
-def test_only_evens():
-    test_list: list[int] = [50, 60, 70, 80, 90, 100]
-    assert only_evens(test_list) == [60, 80, 100]
-
-def test_only_evens():
-    test_list: list[int] = [5, 7, 9]
-    assert only_evens(test_list) == []
 
 def test_concat():
-    test_first_list: list[int] = [1, 2, 3]
-    test_second_list: list[int] = [1, 2, 3]
-    assert concat(test_first_list + test_second_list) == [1, 2, 3, 1, 2, 3]
+    test_first_list_1: list[int] = [1, 2, 3]
+    test_second_list_1: list[int] = [1, 2, 3]
+    assert concat(test_first_list_1 + test_second_list_1) == [1, 2, 3, 1, 2, 3]
+    test_first_list_2: list[int] = [100, 100, 100]
+    test_second_list_2: list[int] = [200, 200, 200]
+    assert concat(test_first_list_2 + test_second_list_2) == [100, 100, 100, 200, 200, 200]
+    test_first_list_3: list[int] = [0, 0, -1]
+    test_second_list_3: list[int] = [-1, 0, 0]
+    assert concat(test_first_list_3 + test_second_list_3) == [0, 0, -1, -1, 0, 0]
 
-def test_concat():
-    test_first_list: list[int] = [100, 100, 100]
-    test_second_list: list[int] = [200, 200, 200]
-    assert concat(test_first_list + test_second_list) == [100, 100, 100, 200, 200, 200]
-
-def test_concat():
-    test_first_list: list[int] = [0, 0, -1]
-    test_second_list: list[int] = [-1, 0, 0]
-    assert concat(test_first_list + test_second_list) == [0, 0, -1, -1, 0, 0]
 
 def test_sub():
-    test_first: list[int] = [10, 20, 30, 40]
-    start_index: list[int] = [20]
-    end_index:  list[int] = [30]
-    assert sub(test_first[start_index: end_index]) == [20, 30]
-
-def test_sub():
-    test_first: list[int] = [1, 2, 3, 4]
-    start_index: list[int] = [3]
-    end_index:  list[int] = [4]
-    assert sub(test_first[start_index: end_index]) == [3, 4]
-
-def test_sub():
-    test_first: list[int] = [0, -1, 2, 3]
-    start_index: list[int] = [-1]
-    end_index:  list[int] = [2]
-    assert sub(test_first[start_index: end_index]) == [-1, 2]
+    test_first_1: list[int] = [10, 20, 30, 40]
+    start_index_1: list[int] = [20]
+    end_index_1: list[int] = [30]
+    assert sub(test_first_1[start_index_1: end_index_1]) == [20, 30]
+    test_first_2: list[int] = [1, 2, 3, 4]
+    start_index_2: list[int] = [3]
+    end_index_2: list[int] = [4]
+    assert sub(test_first_2[start_index_2: end_index_2]) == [3, 4]
+    test_first_3: list[int] = [0, -1, 2, 3]
+    start_index_3: list[int] = [-1]
+    end_index_3: list[int] = [2]
+    assert sub(test_first_3[start_index_3: end_index_3]) == [-1, 2]
